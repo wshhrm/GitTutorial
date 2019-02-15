0a99fdc8c98a1e5ae9648ed4d34c280f5d14bb8f
Tianrun <wshhrm@163.com>
2019-02-16 00:06:53 +1100

    added another print

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("hello cs193p")
         print("helloworld")
         super.viewDidLoad()
 
